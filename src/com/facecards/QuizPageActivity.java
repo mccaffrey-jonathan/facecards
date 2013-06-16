0475b817132d81ddef1562d845553b2bfac7b44d
Alexey Komissarouk <AlexeyMK@gmail.com>
2013-06-16 14:47:06 -0700

    miracle of miracles, when we get something correct we remove the proper item!

--- src/com/facecards/QuizPageActivity.java
+++ src/com/facecards/QuizPageActivity.java
@@ -19,6 +19,8 @@ import android.widget.Toast;
 
 import com.facebook.widget.ProfilePictureView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QuizPageActivity extends Activity {
@@ -32,27 +34,27 @@ public class QuizPageActivity extends Activity {
 
         Intent startingIntent = getIntent();
 
-        final String[] names = startingIntent.getStringArrayExtra("names");
-        String[] fb_ids = startingIntent.getStringArrayExtra("uids");
+        final List<String> names = new ArrayList<String>(Arrays.asList(startingIntent.getStringArrayExtra("names")));
+        final List<String> fb_ids = new ArrayList<String>(Arrays.asList(startingIntent.getStringArrayExtra("uids")));
 
 //        String[] names = new String[] {"John", "Bob", "Sarah"};
 //        String[] fb_ids = new String[] {"618833", "4"};
 
-        ArrayAdapter names_adapter = new ArrayAdapter<String>(this, R.layout.quiz_name, names);
-        GridView names_view = (GridView) findViewById(R.id.quiz_names);
+        final ArrayAdapter names_adapter = new ArrayAdapter<String>(this, R.layout.quiz_name, names);
+        final GridView names_view = (GridView) findViewById(R.id.quiz_names);
         names_view.setAdapter(names_adapter);
         names_view.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View child_view, int i, long l) {
-                CharSequence drag_content = ((TextView)child_view).getText().toString();
+                CharSequence drag_content = ((TextView) child_view).getText().toString();
                 ClipData.Item item = new ClipData.Item(drag_content);
-                String[] content_types = new String[] { ClipDescription.MIMETYPE_TEXT_PLAIN};
+                String[] content_types = new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN};
                 ClipData dragData = new ClipData(drag_content, content_types, item);
                 View.DragShadowBuilder myShadow = new NameDragShadowBuilder(child_view);
                 child_view.startDrag(dragData,  // the data to be dragged
-                    myShadow,  // the drag shadow builder
-                    null,      // no need to use local data
-                    0          // flags (not currently used, set to 0)
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        0          // flags (not currently used, set to 0)
                 );
                 return true;
             }
@@ -61,7 +63,7 @@ public class QuizPageActivity extends Activity {
         final GridView photos_view = (GridView) findViewById(R.id.quiz_photos);
 
         class ProfilePictureArrayAdapter<String> extends ArrayAdapter<String> {
-            public ProfilePictureArrayAdapter(Context c, int r, String[] items) {
+            public ProfilePictureArrayAdapter(Context c, int r, List<String> items) {
                 super(c, r, items);
             }
 
@@ -73,13 +75,19 @@ public class QuizPageActivity extends Activity {
                         @Override
                         public boolean onDrag(View view, DragEvent dragEvent) {
                             if (dragEvent.getAction() == DragEvent.ACTION_DROP) {
-                                boolean correct = view.getTag().equals(
-                                                  dragEvent.getClipData().getItemAt(0).getText());
+                                CharSequence drag_text = dragEvent.getClipData().getItemAt(0).getText();
+                                boolean correct = view.getTag().equals(drag_text);
                                 Toast.makeText(getContext(), correct ? "Correct!" : "Nope", 2).show();
-                                Log.i("AMK", "Dropped into me: " + Integer.toString(position));
-                                ProfilePictureArrayAdapter.this.remove((String)view.getTag());
-                                ProfilePictureArrayAdapter.this.notifyDataSetChanged();
-                                photos_view.invalidateViews();
+                                Log.i("AMK", "Dropped " + drag_text + " into " + view.getTag());
+                                if (correct) {
+                                    ProfilePictureView v = (ProfilePictureView) view;
+                                    ProfilePictureArrayAdapter.this.remove((String)v.getProfileId());
+                                    ProfilePictureArrayAdapter.this.notifyDataSetChanged();
+                                    photos_view.invalidateViews();
+                                    names_adapter.remove(view.getTag());
+                                    names_adapter.notifyDataSetChanged();
+                                    names_view.invalidateViews();
+                                }
                                 return true;
                             } else if (dragEvent.getAction() == DragEvent.ACTION_DRAG_STARTED) {
                                 return true;   // Yeah, I'm down to take a drop event.
@@ -88,9 +96,12 @@ public class QuizPageActivity extends Activity {
                             }
                         }
                     });
-                    // save correct name on view
-                    view.setTag(names[position]);
                 }
+                // save correct name on view
+                Log.i("AMK", "Saving #" + Integer.toString(position) + " (name " + names.get(position) + ")");
+                Log.i("AMK", "Previous Tag: " + view.getTag());
+                view.setTag(names.get(position));
+
                 ProfilePictureView pic_view = (ProfilePictureView) view;
                 pic_view.setProfileId((java.lang.String)this.getItem(position));
                 return pic_view;
