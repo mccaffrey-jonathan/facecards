e85b4d9c609e144a3d55ea1885abccbd3879e855
jmccaf <mccaffrey.jonathan@gmail.com>
2013-06-15 19:48:47 -0700

    pull uid

--- src/com/facecards/SplashActivity.java
+++ src/com/facecards/SplashActivity.java
@@ -1,17 +1,75 @@
 package com.facecards;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 
 public class SplashActivity extends Activity {
+
+    private static final String TAG = "SplashActivity";
+    private UiLifecycleHelper uiHelper;
+
+    private Activity getActivity() {
+        return this;
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+        }
+    }
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
 
         // Inflate our UI from its XML layout description.
         setContentView(R.layout.splash_activity);
-
     }
 
+    @Override
+        public void onResume() {
+            super.onResume();
+            uiHelper.onResume();
+        }
+
+    @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+    @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+    @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+    @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
 }
