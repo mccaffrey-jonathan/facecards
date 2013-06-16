7f872d2da0b3e6b65a699ebc419449cde4924230
Alexey Komissarouk <AlexeyMK@gmail.com>
2013-06-15 23:04:40 -0700

    basic version of quiz, working on dragging next.

--- src/com/facecards/QuizPageActivity.java
+++ src/com/facecards/QuizPageActivity.java
@@ -1,7 +1,11 @@
 package com.facecards;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
@@ -24,12 +28,27 @@ public class QuizPageActivity extends Activity {
         GridView names_view = (GridView) findViewById(R.id.quiz_names);
         names_view.setAdapter(names_adapter);
 
+        class ProfilePictureArrayAdapter<String> extends ArrayAdapter<String> {
+            public ProfilePictureArrayAdapter(Context c, int r, String[] items) {
+                super(c, r, items);
+            }
+
+            public View getView(int position, View view, ViewGroup group) {
+                if (view == null) {
+                    LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    view = vi.inflate(R.layout.quiz_photo, null);
+                }
+                ProfilePictureView pic_view = (ProfilePictureView) view;
+                pic_view.setProfileId((java.lang.String)this.getItem(position));
+                return pic_view;
+            }
+        }
+
         String[] fb_ids = new String[] {"618833", "4"};
-        ArrayAdapter photos_adapter = new ArrayAdapter<String>(this,
-            R.layout.quiz_photo, fb_ids);
+        ProfilePictureArrayAdapter<String> photos_adapter = new ProfilePictureArrayAdapter<String>(
+            this, R.layout.quiz_photo, fb_ids);
         GridView photos_view = (GridView) findViewById(R.id.quiz_photos);
         photos_view.setAdapter(photos_adapter);
-
     }
 
 }
